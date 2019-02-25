bc3ad28d355b661a984640b3b557fcca96c8aa87
Denis Alayza <denis.alayza@gmail.com>
2019-02-25 13:54:52 -0300

    Seccion 5 - Data desde archivos externos

---
+++ Ejercicio_04/EjeciciohttpRequest/js/app.js
@@ -0,0 +1,22 @@
+(function(){
+
+var app = angular.module('ejemplosApp',[ ]);
+
+
+
+app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+  
+    $scope.profesores = [];
+
+    $http.get('json/profesores.json').success(function(data){
+        // Codigo cuando es correcta la peticion
+        $scope.profesores = data.profesores;
+    });
+
+}]);
+
+
+
+
+
+})();
