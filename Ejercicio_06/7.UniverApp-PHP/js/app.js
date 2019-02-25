cbd1e74ff9b7b2ebe3fbbf3019059b94b144db89
Denis Alayza <denis.alayza@gmail.com>
2019-02-25 15:44:02 -0300

    Seccion 6 - Sitios Dinamicos - Responsive Sites

---
+++ Ejercicio_06/7.UniverApp-PHP/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('universidadApp',['ngRoute']);
+
+
+app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+  
+	$scope.menuSuperior = 'parciales/menu.html';
+
+
+	$scope.setActive = function(Opcion){
+
+		$scope.mInicio     = "";
+		$scope.mProfesores = "";
+		$scope.mAlumnos    = "";
+
+		$scope[Opcion] = "active";
+
+	}
+
+}]);
+
+
+// filtro personalizado de telefono
+app.filter('telefono', function() {
+	return function(numero) {
+		return numero.substring(0,4) + "-" + numero.substring(4);
+	}
+})
